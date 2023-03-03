ce2596d9518940f07db5a5a0dca6737c8da0ec54
ruichengeng <90064166+ruichengeng@users.noreply.github.com>
2023-03-03 13:38:57 -0500

    added bomberman sprite and fixed animation + movement

--- NEXT-API/GameTest/GameTest.cpp
+++ NEXT-API/GameTest/GameTest.cpp
@@ -29,14 +29,14 @@ void Init()
 {
 	//------------------------------------------------------------------------
 	// Example Sprite Code....
-	testSprite = App::CreateSprite(".\\TestData\\Test.bmp", 8, 4);
+	testSprite = App::CreateSprite(".\\Art\\NeoEarlyBomberman.bmp", 7, 4);
 	testSprite->SetPosition(400.0f, 400.0f);
 	float speed = 1.0f / 15.0f;
-	testSprite->CreateAnimation(ANIM_BACKWARDS, speed, { 0,1,2,3,4,5,6,7 });
-	testSprite->CreateAnimation(ANIM_LEFT, speed, { 8,9,10,11,12,13,14,15 });
-	testSprite->CreateAnimation(ANIM_RIGHT, speed, { 16,17,18,19,20,21,22,23 });
-	testSprite->CreateAnimation(ANIM_FORWARDS, speed, { 24,25,26,27,28,29,30,31 });
-	testSprite->SetScale(1.0f);
+	testSprite->CreateAnimation(ANIM_BACKWARDS, speed, { 0,1,2,3,4,5,6 });
+	testSprite->CreateAnimation(ANIM_RIGHT, speed, { 7,8,9,10,11,12,13 });
+	testSprite->CreateAnimation(ANIM_FORWARDS, speed, { 14,15,16,17,18,19,20 });
+	testSprite->CreateAnimation(ANIM_LEFT, speed, { 21,22,23,24,25,26,27 });
+	testSprite->SetScale(1.5f);
 	//------------------------------------------------------------------------
 }
 
@@ -49,38 +49,48 @@ void Update(float deltaTime)
 	//------------------------------------------------------------------------
 	// Example Sprite Code....
 	testSprite->Update(deltaTime);
-	if (App::GetController().GetLeftThumbStickX() > 0.5f)
+
+	if ((App::GetController().GetLeftThumbStickX() != 0.0f) || (App::GetController().GetLeftThumbStickY() != 0.0f))
 	{
-		testSprite->SetAnimation(ANIM_RIGHT);
-		float x, y;
-		testSprite->GetPosition(x, y);
-		x += 1.0f;
-		testSprite->SetPosition(x, y);
+		if (App::GetController().GetLeftThumbStickX() > 0.5f)
+		{
+			testSprite->SetAnimation(ANIM_RIGHT);
+			float x, y;
+			testSprite->GetPosition(x, y);
+			x += 1.0f;
+			testSprite->SetPosition(x, y);
+		}
+		if (App::GetController().GetLeftThumbStickX() < -0.5f)
+		{
+			testSprite->SetAnimation(ANIM_LEFT);
+			float x, y;
+			testSprite->GetPosition(x, y);
+			x -= 1.0f;
+			testSprite->SetPosition(x, y);
+		}
+		if (App::GetController().GetLeftThumbStickY() > 0.5f)
+		{
+			testSprite->SetAnimation(ANIM_FORWARDS);
+			float x, y;
+			testSprite->GetPosition(x, y);
+			y -= 1.0f;
+			testSprite->SetPosition(x, y);
+		}
+		if (App::GetController().GetLeftThumbStickY() < -0.5f)
+		{
+			testSprite->SetAnimation(ANIM_BACKWARDS);
+			float x, y;
+			testSprite->GetPosition(x, y);
+			y += 1.0f;
+			testSprite->SetPosition(x, y);
+		}
 	}
-	if (App::GetController().GetLeftThumbStickX() < -0.5f)
+	else
 	{
-		testSprite->SetAnimation(ANIM_LEFT);
-		float x, y;
-		testSprite->GetPosition(x, y);
-		x -= 1.0f;
-		testSprite->SetPosition(x, y);
+		testSprite->SetAnimation(-1);
 	}
-	if (App::GetController().GetLeftThumbStickY() > 0.5f)
-	{
-		testSprite->SetAnimation(ANIM_FORWARDS);
-		float x, y;
-		testSprite->GetPosition(x, y);
-		y += 1.0f;
-		testSprite->SetPosition(x, y);
-	}
-	if (App::GetController().GetLeftThumbStickY() < -0.5f)
-	{
-		testSprite->SetAnimation(ANIM_BACKWARDS);
-		float x, y;
-		testSprite->GetPosition(x, y);
-		y -= 1.0f;
-		testSprite->SetPosition(x, y);
-	}
+
+
 	if (App::GetController().CheckButton(XINPUT_GAMEPAD_DPAD_UP, false))
 	{
 		testSprite->SetScale(testSprite->GetScale() + 0.1f);
